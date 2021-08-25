3e0ebe4a8ef5984e885f52895e42f3b6d6fae40a
DenisB <dmbogush91@gmail.com>
2021-08-25 12:10:09 +0300

    add draft todo app

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDawvuvAZmkBheTTRdB6_X9idvMC5yVaDs",
+    authDomain: "habit-tracker-324008.firebaseapp.com",
+    projectId: "habit-tracker-324008",
+    storageBucket: "habit-tracker-324008.appspot.com",
+    messagingSenderId: "1091835775630",
+    appId: "1:1091835775630:web:6af399573346896fdc57df",
+    databaseURL: "https://habit-tracker-324008-default-rtdb.europe-west1.firebasedatabase.app/",
+};
+
+firebase.initializeApp(firebaseConfig);
+export default firebase;
